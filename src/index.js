ef9ba7948c4282cab396be9cc3c4916d473ae0bd
Nick Petrenko <extendsnull@gmail.com>
2020-10-24 13:25:07 +0300

    init: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const bracketsPairs = Object.fromEntries(bracketsConfig);
+
+    const temp = str.split('').reduce((accumulator, current) => {
+        if (accumulator[0] === current) {
+            accumulator.shift();
+        } else if (bracketsPairs[current]) {
+            accumulator.unshift(bracketsPairs[current]);
+        } else {
+            accumulator.push(false);
+        }
+
+        return accumulator;
+    }, []);
+
+    return !temp.length;
 }
